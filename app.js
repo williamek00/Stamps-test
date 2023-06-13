a9bc48d087dbcda66b24077ee792110a2ff4d03f
williamek00 <williamenosk123@gmail.com>
2023-06-13 22:16:56 +0700

    fixing bugs weather function

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 //1. Program ApaBole
 
-let output = ""; //declare variabel penampung
+let output = ""; //declare variable
 
-for (let i = 1; i <= 100; i++) { //looping dari 1 sampai 100
-    if (i % 3 === 0 && i % 5 === 0)  output += "ApaBole, "; // kondisional
+for (let i = 1; i <= 100; i++) { //loop from 1 to 100
+    if (i % 3 === 0 && i % 5 === 0) output += "ApaBole, "; // conditional
     else if (i % 3 === 0) output += "Apa, ";
     else if (i % 5 === 0) output += "Bole, ";
     else output += i + ", ";
 }
 
-console.log(output); //menampilkan ke terminal
+console.log(output);
 
 //2.Weather app
 // get data with axios
 const axios = require('axios')
 // import helper 
-const {formatDate,temperatureFormat} = require('./helper')
+const { formatDate, temperatureFormat } = require('./helper')
 // define data for 3rd api
 const lat = -6.200000
 const lon = 106.816666
@@ -25,18 +25,20 @@ const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${l
 async function getWeather() {
     try {
         //get data from 3rd api
-      let response = await axios.get(url);
-      //store data to a variable
-      let results = response.data;
-      //loop results to get dynamic data
-      results.list.forEach((e) => {
-        console.log(formatDate(e.dt_txt)+': '+temperatureFormat(e.main.temp));
-      });
+        let response = await axios.get(url);
+        //store data to a variable
+        let results = response.data;
+        //loop results to get dynamic data
+        results.list.forEach((e) => {
+            //use helpers for formatting
+            console.log(formatDate(e.dt_txt) + ': ' + temperatureFormat(e.main.temp));
+        });
+
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
-  }
-  
+}
+
 
 getWeather()
 
