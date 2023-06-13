2d9e17ff0d34e7f9766909e7e0e7275e09038459
williamek00 <williamenosk123@gmail.com>
2023-06-13 22:07:27 +0700

    feat add hints

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ for (let i = 1; i <= 100; i++) { //looping dari 1 sampai 100
 
 console.log(output); //menampilkan ke terminal
 
+//2.Weather app
+// get data with axios
 const axios = require('axios')
+// import helper 
 const {formatDate,temperatureFormat} = require('./helper')
+// define data for 3rd api
 const lat = -6.200000
 const lon = 106.816666
 const APIkey = '1fd1ee78d2d5f3ed4f8eec8d3ac71def'
@@ -20,9 +24,12 @@ const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${l
 
 async function getWeather() {
     try {
+        //get data from 3rd api
       let response = await axios.get(url);
-      let result = response.data;
-      result.list.forEach((e) => {
+      //store data to a variable
+      let results = response.data;
+      //loop results to get dynamic data
+      results.list.forEach((e) => {
         console.log(formatDate(e.dt_txt)+': '+temperatureFormat(e.main.temp));
       });
     } catch (error) {
